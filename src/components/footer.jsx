1f7b213de64cf2b836ec89ce38252f145d87b11c
taoyeming <taoyeming@192.168.1.7>
2024-09-01 11:35:42 +0800

    部分修改

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,7 +6,7 @@ const { Footer } = Layout;
 const Component = () => {
   return <Footer className='p-2'>
     <a className='ml-auto mr-auto w-fit flex' href="https://beian.miit.gov.cn">
-      浙ICP备2024107647号-1
+      浙ICP备2024107647号-3
     </a>
   </Footer>
 }
