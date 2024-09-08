0f27208268a0f2064fe5df7aa6c3b686a941aeb7
taoyeming <taoyeming@192.168.1.7>
2024-09-08 15:36:19 +0800

    部分修改

--- src/components/follow.jsx
+++ src/components/follow.jsx
@@ -10,22 +10,18 @@ const Component = () => {
 
         <div className="flex justify-around mb-4 max-w-[800px] mr-auto ml-auto">
             <div className='flex-1 mr-4'>
-                <img src={douyin} className="w-full" alt="gzh" />
-                <span>公众号：搞机分享汇</span>
-            </div>
-            <div className='flex-1 mr-4'>
-                <img src={fenxianghui} className="w-full" alt="wx" />
-                <span>微信：iOSRE-Erik</span>
-            </div>
-            <div className='flex-1'>
-                <img src={weixin} className="w-full" alt="dy" />
+                <img src={douyin} className="w-full" alt="dy" />
                 <span>抖音：搞机分享汇</span>
             </div>
+            <div className='flex-1 mr-4'>
+                <img src={fenxianghui} className="w-full" alt="gzh" />
+                <span>公众号：搞机分享汇</span>
+            </div>
+            <div className='flex-1'>
+                <img src={weixin} className="w-full" alt="wx" />
+                <span>微信：iOSRE-Erik</span>
+            </div>
         </div>
-        <span>
-            一条略懂iOS正逆向开发的咸鱼，接iOS软件开发，插件定制（App Store内插件定制勿扰）<br/>
-            其他搞机需求可联系详谈包括但不限于PC,Switch,手机等软硬件需求
-        </span>
     </div>
 }
 
