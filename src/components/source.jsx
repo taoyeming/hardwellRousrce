b0dc32894727404a9c1bcb703ed70caf37394978
陶冶明 <taoyeming.tym@alibaba-inc.com>
2024-10-10 11:41:10 +0800

    feat: 资源列表

--- src/components/source.jsx
+++ src/components/source.jsx
@@ -1,6 +1,7 @@
 import { Card, Tag, Modal } from "antd";
 import { LinkOutlined } from '@ant-design/icons'
 import React from "react";
+import { sourceData as items } from '../utils/data'
 
 const Item = ({title, desc, tag, url, password}) => {
     return <Card 
@@ -36,35 +37,6 @@ const Item = ({title, desc, tag, url, password}) => {
 }
 
 const Component = () => {
-
-    const items = [
-        {
-            title: 'App加锁助手',
-            desc: '一款提供越狱iOS设备App加锁的工具，支持多种加锁方式，支持iOS14-16设备有根及无根环境，已上传BigBoss及搞机软件源。',
-            tag: 'iOS插件',
-            url: 'https://apt.wanjitech.com/depictions/index.html?packageId=com.erik.AppLockPro'
-        },
-        {
-            title: 'WinPE',
-            desc: '一款简洁的PE启动盘制作工具。',
-            tag: '网站',
-            url: 'https://www.wepe.com.cn/download.html'
-        },
-        {
-            title: 'CS扫描全能王',
-            desc: '扫描全能王解锁功能限制安装包。',
-            tag: 'iOS软件',
-            url: 'https://caiyun.139.com/m/i?165CkuGx9Xmxp',
-            password: "4z6L",
-        },
-        {
-            title: 'MSDN',
-            desc: '一个原版Windows系统下载网站。',
-            tag: '网站',
-            url: 'https://msdn.itellyou.cn'
-        }
-    ]
-
     return <div>
         {items.map(i => {
             return <Item {...i} />
