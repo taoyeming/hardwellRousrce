b84bb443fecaa90ae3181779bac4d029695985f2
haowenkai <haowenkai@zhiyitech.cn>
2024-10-27 15:44:21 +0800

    build: 更新资源

--- src/utils/data.js
+++ src/utils/data.js
@@ -24,6 +24,13 @@ export const sourceData = [
         desc: '一个原版Windows系统下载网站。',
         tag: '网站',
         url: 'https://msdn.itellyou.cn'
+    },
+    {
+        title: 'NOMO RAW相机',
+        desc: 'NOMO RAW相机解锁功能限制安装包。',
+        tag: 'iOS软件',
+        url: 'https://caiyun.139.com/m/i?2i3pdz0afigu3',
+        password: "r95w"
     }
 ]
 
@@ -31,8 +38,8 @@ export const sourceData = [
  * 首页提示框，如果没有设置为null
  */
 export const homeAlertDetail = {
-    content: '测试提示',
-    url: 'http://www.baidu.com',
+    content: '二手全系苹果手机出售，可选是否支持越狱，一对一包越狱，公众号联系或添加微信：iOSRE-Erik，提出需求系统型号等要求报价，仅想听报价无意要勿扰',
+    url: null
 }
 
 /**
@@ -43,15 +50,15 @@ export const wallpaperData = [
         label: '手机壁纸',
         key: '手机壁纸',
         picturelist: [
-            "https://wx.wanjitech.com/bizhi/10.JPG",
-            "https://wx.wanjitech.com/bizhi/11.JPG",
-            "https://wx.wanjitech.com/bizhi/12.JPG",
-            "https://wx.wanjitech.com/bizhi/13.JPG",
-            "https://wx.wanjitech.com/bizhi/14.JPG",
-            "https://wx.wanjitech.com/bizhi/15.JPG",
-            "https://wx.wanjitech.com/bizhi/16.JPG",
-            "https://wx.wanjitech.com/bizhi/17.JPG",
-            "https://wx.wanjitech.com/bizhi/18.JPG"
+            "https://wx.wanjitech.com/bizhi/1.JPG",
+            "https://wx.wanjitech.com/bizhi/2.JPG",
+            "https://wx.wanjitech.com/bizhi/3.JPG",
+            "https://wx.wanjitech.com/bizhi/4.JPG",
+            "https://wx.wanjitech.com/bizhi/5.JPG",
+            "https://wx.wanjitech.com/bizhi/6.JPG",
+            "https://wx.wanjitech.com/bizhi/7.JPG",
+            "https://wx.wanjitech.com/bizhi/8.JPG",
+            "https://wx.wanjitech.com/bizhi/9.JPG",
         ]
     },
     {
