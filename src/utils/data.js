3cfb91ecf1c69952004396d09b5b9792ebcf5a80
haowenkai <haowenkai@zhiyitech.cn>
2024-11-17 14:55:38 +0800

    feat: 补充资源

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,36 +1,178 @@
 
 export const sourceData = [
     {
-        title: 'App加锁助手',
-        desc: '一款提供越狱iOS设备App加锁的工具，支持多种加锁方式，支持iOS14-16设备有根及无根环境，已上传BigBoss及搞机软件源。',
-        tag: 'iOS插件',
-        url: 'https://apt.wanjitech.com/depictions/index.html?packageId=com.erik.AppLockPro'
+        "title": "App加锁助手",
+        "desc": "一款提供越狱iOS设备App加锁的工具，支持多种加锁方式，支持iOS14-16设备有根及无根环境，已上传BigBoss及搞机软件源。",
+        "tag": "iOS插件",
+        "url":"https://apt.wanjitech.com"
     },
     {
-        title: 'WinPE',
-        desc: '一款简洁的PE启动盘制作工具。',
-        tag: '网站',
-        url: 'https://www.wepe.com.cn/download.html'
+        "title": "App内部锁",
+        "desc": "一款提供越狱iOS设备App加锁的工具，支持图形锁，支持iOS14-16设备有根及无根环境，已上传BigBoss及搞机软件源。",
+        "tag": "iOS插件",
+        "url":"https://apt.wanjitech.com"
     },
     {
-        title: 'CS扫描全能王',
-        desc: '扫描全能王解锁功能限制安装包。',
-        tag: 'iOS软件',
-        url: 'https://caiyun.139.com/m/i?165CkuGx9Xmxp',
-        password: "4z6L",
+        "title": "App隐藏助手",
+        "desc": "一款提供越狱iOS设备App隐藏的工具，一键隐藏你安装的各种应用，支持iOS14-16设备有根及无根环境，已上传BigBoss及搞机软件源。",
+        "tag": "iOS插件",
+        "url":"https://apt.wanjitech.com"
     },
     {
-        title: 'MSDN',
-        desc: '一个原版Windows系统下载网站。',
-        tag: '网站',
-        url: 'https://msdn.itellyou.cn'
+        "title": "小决定",
+        "desc": "小决定解锁Pro功能。",
+        "tag": "iOS软件",
+        "url":"https://caiyun.139.com/m/i?2i3pd0XGoFB4l",
+        "password":"yuyd"
     },
     {
-        title: 'NOMO RAW相机',
-        desc: 'NOMO RAW相机解锁功能限制安装包。',
-        tag: 'iOS软件',
-        url: 'https://caiyun.139.com/m/i?2i3pdz0afigu3',
-        password: "r95w"
+        "title": "NOMO RAW相机",
+        "desc": "NOMO RAW相机，解锁功能限制。",
+        "tag": "iOS软件",
+        "url":"https://caiyun.139.com/m/i?2i3pdz0afigu3",
+        "password":"r95w"
+    },
+    {
+        "title": "拦截猫",
+        "desc": "基于AI的一款骚扰拦截App，解锁功能限制。",
+        "tag": "iOS软件",
+        "url":"https://caiyun.139.com/m/i?165Cjrp8bIOD4",
+        "password":"B9Xs"
+    },
+    {
+        "title": "视频转文字",
+        "desc": "视频转文字神器，支持无限制使用各种配音。",
+        "tag": "iOS软件",
+        "url":"https://caiyun.139.com/m/i?165CkFCSM777K",
+        "password":"evyA"
+    },
+    {
+        "title": "CS扫描全能王",
+        "desc": "扫描全能王解锁功能限制安装包。",
+        "tag": "iOS软件",
+        "url":"https://caiyun.139.com/m/i?165CkuGx9Xmxp",
+        "password":"4z6L"
+    },
+    {
+        "title": "iScreen我的桌面",
+        "desc": "丰富小组件App，解锁VIP，先安装原版App，然后巨魔安装覆盖否则无法获取权限。",
+        "tag": "iOS软件",
+        "url":"https://caiyun.139.com/m/i?165CdBJCDHp2t",
+        "password":"Tv87"
+    },
+    {
+        "title": "ColorfulWidgetVIP版",
+        "desc": "丰富小组件App，解锁VIP，巨魔安装否则无法使用小组件。",
+        "tag": "iOS软件",
+        "url":"https://caiyun.139.com/m/i?165CdCQeHLMra",
+        "password":"4sjq"
+    },
+    {
+        "title": "快影VIP版",
+        "desc": "快手旗下剪辑工具，解锁VIP。",
+        "tag": "iOS软件",
+        "url":"https://pan.quark.cn/s/40644c1df98e"
+    },
+    {
+        "title": "555影视",
+        "desc": "老牌影视app去除内部广告。",
+        "tag": "iOS软件",
+        "url":"https://pan.quark.cn/s/4d180743f618"
+    },
+    {
+        "title": "555网站",
+        "desc": "老牌影视网站防丢失主页。",
+        "tag": "影视网站",
+        "url":"https://w55xy.com/"
+    },
+    {
+        "title": "无他相机",
+        "desc": "无他相机解锁VIP。",
+        "tag": "iOS软件",
+        "url":"https://pan.quark.cn/s/75bbce74ab02"
+    },
+    {
+        "title": "B612相机",
+        "desc": "B612相机无功能限制。",
+        "tag": "iOS软件",
+        "url":"https://pan.quark.cn/s/a3104629db8f"
+    },
+    {
+        "title": "Beautyplus Video",
+        "desc": "外区AI辅助修图神器Pro版，无功能限制。",
+        "tag": "iOS软件",
+        "url":"https://pan.quark.cn/s/305fee7ebd8b"
+    },
+    {
+        "title": "消除笔",
+        "desc": "消除笔去除功能限制版本，解锁VIP功能。",
+        "tag": "iOS软件",
+        "url":"https://pan.quark.cn/s/1a2a03e2c834"
+    },
+    {
+        "title": "一甜相机",
+        "desc": "一甜相机去除vip限制，无限制使用各种功能。",
+        "tag": "iOS软件",
+        "url":"https://pan.quark.cn/s/2c927f432bf9"
+    },
+    {
+        "title": "DLabel云标签",
+        "desc": "云标签支持打印各种标签软件，无限制使用各种VIP功能。",
+        "tag": "iOS软件",
+        "url":"https://url79.ctfile.com/f/20020879-995613304-7428b5?p=5080",
+        "password":"5080"
+    },
+    {
+        "title": "西窗烛",
+        "desc": "一款诗词赏析App，无限制阅读下载书籍。",
+        "tag": "iOS软件",
+        "url":"https://url79.ctfile.com/f/20020879-1008326917-ffcce3?p=5080",
+        "password":"5080"
+    },
+    {
+        "title": "Sleep Cycle",
+        "desc": "一款国外闹钟App，解锁所有功能限制。",
+        "tag": "iOS软件",
+        "url":"https://url79.ctfile.com/f/20020879-1011797920-b52f73?p=5080",
+        "password":"5080"
+    },
+    {
+        "title": "实时翻译官",
+        "desc": "一款实时翻译App，支持各种语言的对话翻译，无限制使用全部功能。",
+        "tag": "iOS软件",
+        "url":"https://url79.ctfile.com/f/20020879-1015248475-0b50a7?p=5080",
+        "password":"5080"
+    },
+    {
+        "title": "小巧工具箱",
+        "desc": "一款各种功能集成的App，内含多种功能。",
+        "tag": "iOS软件",
+        "url":"https://url79.ctfile.com/f/20020879-1027845454-2cc304?p=5080",
+        "password":"5080"
+    },
+    {
+        "title": "WinPE",
+        "desc": "一款简洁的PE启动盘制作工具。",
+        "tag": "网站",
+        "url":"https://www.wepe.com.cn/download.html"
+    },
+    {
+        "title": "MSDN",
+        "desc": "一个原版Windows系统下载网站。",
+        "tag": "网站",
+        "url":"https://msdn.itellyou.cn"
+    },
+    {
+        "title": "小霸王",
+        "desc": "一个在线游戏模拟器支持各种模拟器。",
+        "tag": "网站",
+        "url":"https://www.yikm.net/"
+    },
+    {
+        "title": "资源避难所",
+        "desc": "提供了各种PC，Switch游戏等资源合集。",
+        "tag": "网站",
+        "url":"http://flysheep.ysepan.com/"
     }
 ]
 
